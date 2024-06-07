1c26dc7d7cf9f126768eeb7da5e7675ad04edb0a
Farhang <46557204+farhangnaderi@users.noreply.github.com>
2024-06-06 21:59:21 -0400

    Draft #1

--- src/mvp_control/thruster_ros.cpp
+++ src/mvp_control/thruster_ros.cpp
@@ -18,7 +18,11 @@
     Email: emircem@uri.edu;emircem.gezer@gmail.com
     Year: 2022
 
-    Copyright (C) 2022 Smart Ocean Systems Laboratory
+    Author: Farhang Naderi
+    Email: farhang.naderi@uri.edu;farhang.nba@gmail.com
+    Year: 2024
+
+    Copyright (C) 2024 Smart Ocean Systems Laboratory
 */
 
 #include "thruster_ros.h"
@@ -29,8 +33,12 @@
 
 using namespace ctrl;
 
-ThrusterROS::ThrusterROS() {
+ThrusterROS::ThrusterROS() 
+        : m_nh() ,
+        m_pnh("~")
+{
     m_poly_solver.reset(new PolynomialSolver());
+
 }
 
 ThrusterROS::ThrusterROS(std::string id, std::string topic_id, Eigen::VectorXd contribution_vector) :
@@ -61,6 +69,29 @@ void ThrusterROS::set_thrust_force_topic_id(const decltype(m_thrust_force_topic_
     m_thrust_force_topic_id = topic_id;
 }
 
+auto ThrusterROS::get_servo_command_topic_id() -> decltype(m_servo_command_topic_id) {
+    return m_servo_command_topic_id;
+}
+
+void ThrusterROS::set_servo_command_topic_id(const decltype(m_servo_command_topic_id) &topic_id) {
+    m_servo_command_topic_id = topic_id;
+}
+auto ThrusterROS::get_joint_state_topic_id() -> decltype(m_joint_state_topic_id) {
+    return m_joint_state_topic_id;
+}
+
+void ThrusterROS::set_joint_state_topic_id(const decltype(m_joint_state_topic_id) &topic_id) {
+    m_joint_state_topic_id = topic_id;
+}
+
+auto ThrusterROS::get_joint_state_desired_topic_id() -> decltype(m_joint_state_desired_topic_id) {
+    return m_joint_state_desired_topic_id;
+}
+
+void ThrusterROS::set_joint_state_desired_topic_id(const decltype(m_joint_state_desired_topic_id) &topic_id) {
+    m_joint_state_desired_topic_id = topic_id;
+}
+
 auto ThrusterROS::get_id() -> decltype(m_id) {
     return m_id;
 }
@@ -77,21 +108,63 @@ void ThrusterROS::set_contribution_vector(const decltype(m_contribution_vector)&
     m_contribution_vector = contribution_vector;
 }
 
+void ThrusterROS::set_servo_joints(const std::vector<std::string>& joints) {
+    servo_joints = joints;
+}
+
+std::vector<std::string> ThrusterROS::get_servo_joints() const {
+    return servo_joints;
+}
+
+int ThrusterROS::get_is_articulated() const {
+    return is_articulated;
+}
+void ThrusterROS::set_is_articulated(int value) {
+    is_articulated = value;
+}
+
+const std::vector<double>& ThrusterROS::getServoSpeeds() const {
+    return servo_speeds;
+}
+
+void ThrusterROS::setServoSpeeds(const std::vector<double>& speeds) {
+    servo_speeds = speeds;
+}
+
 void ThrusterROS::initialize() {
+
+    if (!m_pnh.hasParam(CONF_THRUSTER_SERVO_SPEEDS)) {
+        throw control_ros_exception("servo_speeds empty");
+    }
 
     if(!m_thrust_command_topic_id.empty()) {
         m_thrust_publisher = m_nh.advertise<std_msgs::Float64>(
             m_thrust_command_topic_id, 100);
     } else {
-        throw control_ros_exception("empty topic name");
+        throw control_ros_exception("empty command topic name");
     }
 
     if(!m_thrust_command_topic_id.empty()) {
          m_force_publisher = m_nh.advertise<std_msgs::Float64>(
              m_thrust_force_topic_id, 100);
     } else {
-        throw control_ros_exception("empty topic name");
+        throw control_ros_exception("empty force topic name");
     }
+
+    if (!m_joint_state_desired_topic_id.empty()) {
+        m_joint_state_publisher = m_nh.advertise<sensor_msgs::JointState>(
+            m_joint_state_desired_topic_id, 100);
+    } else {
+        throw control_ros_exception("empty joint state topic name");
+    }    
+
+    // Initialization for the servo command topic
+    if (!m_servo_command_topic_id.empty()) {
+        m_servo_command_publisher = m_nh.advertise<std_msgs::Float64>(m_servo_command_topic_id, 100);
+    } else {
+        ROS_WARN_STREAM("Servo command topic name is empty, assuming the thruster is not articulated.");
+    }
+
 }
 
 auto ThrusterROS::get_link_id() -> decltype(m_link_id) {
@@ -102,10 +175,24 @@ void ThrusterROS::set_link_id(const decltype(m_link_id)& link_id) {
     m_link_id = link_id;
 }
 
+auto ThrusterROS::get_frame_id() -> decltype(m_frame_id) {
+    return m_frame_id;
+}
+
+void ThrusterROS::set_frame_id(const decltype(m_frame_id)& frame_id) {
+    m_frame_id = frame_id;
+}
+
 void ThrusterROS::command(double cmd) {
     std_msgs::Float64 msg;
     msg.data = cmd;
     m_thrust_publisher.publish(msg);
+}
+
+void ThrusterROS::servo_joint_command(double cmd) {
+    std_msgs::Float64 msg;
+    msg.data = cmd;
+    m_servo_command_publisher.publish(msg);
 }
 
 auto ThrusterROS::get_poly_solver() -> decltype(m_poly_solver) {
@@ -149,4 +236,37 @@ bool ThrusterROS::request_force(double N) {
     }
 
     return true;
+}
+
+bool ThrusterROS::request_joint_angles(const std::string& joint_name, double requested_angle) {
+
+    if (!m_joint_state_publisher) {
+        ROS_ERROR("Joint state publisher is not initialized!");
+        return false;
+    }
+
+    sensor_msgs::JointState joint_state_msg;
+    // joint_state_msg.header.stamp = ros::Time::now(); 
+    joint_state_msg.name.push_back(joint_name);     
+    joint_state_msg.position.push_back(requested_angle);
+
+    m_joint_state_publisher.publish(joint_state_msg); 
+
+    servo_joint_command(normalize_angle(requested_angle));
+
+    return true;
+}
+
+double ThrusterROS::normalize_angle(double angle) {
+    // Range of the input angle
+    double input_min = -3.14, input_max = 3.14;
+
+    // Desired output range
+    double output_min = -1, output_max = 1;
+
+    // Calculate normalized angle using linear transformation
+    double normalized_angle = (angle - input_min) / (input_max - input_min) 
+                * (output_max - output_min) + output_min;
+                
+    return std::max(std::min(normalized_angle, output_max), output_min);
 }
